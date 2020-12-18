8c037a77707fb67301a8231c93b9c30b349c351a
Poornima THangavel <poornimasuba496@gmail.com>
2020-12-18 12:18:53 +0530

    added func2

--- crud/src/com/lti/entities/check.java
+++ crud/src/com/lti/entities/check.java
@@ -2,6 +2,9 @@ package com.lti.entities;
 
 public class check {
 
+	public void func2() {
+		System.out.println("hello");
+	}
 	public static void main(String[] args) {
 		System.out.println("vanakam");
 
